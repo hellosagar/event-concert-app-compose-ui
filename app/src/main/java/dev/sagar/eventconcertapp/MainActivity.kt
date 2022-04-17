cabb5ac09c519358808a7ba59c192d3f74145d49
hellosagar <sagarkhurana00786@gmail.com>
2022-04-17 23:31:39 +0530

    State

--- app/src/main/java/dev/sagar/eventconcertapp/MainActivity.kt
+++ app/src/main/java/dev/sagar/eventconcertapp/MainActivity.kt
@@ -4,58 +4,64 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val fontFamily = FontFamily(
-            Font(R.font.poppins_regular, FontWeight.Normal),
-        )
-
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        color = Color(0xFF101010)
-                    )
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                                                withStyle(
-                                                    style = SpanStyle(
-                                                        color = Color.Cyan,
-                                                        fontSize = 20.sp,
-                                                        textDecoration = TextDecoration.LineThrough
-                                                    )
-                                                ){
-                                                    append("Jetpack")
-                                                }
-                        append("Compose")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    textDecoration = TextDecoration.Underline
-                )
+
+            Column(modifier = Modifier.fillMaxSize()) {
+
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(color = color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+
+                }
             }
 
         }
     }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        })
 }
 
