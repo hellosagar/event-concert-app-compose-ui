cc3bfe87fc113b00fe60f0098f3306fb8ca83f9f
hellosagar <sagarkhurana00786@gmail.com>
2022-04-19 20:19:19 +0530

    List and Lazy columns aka similar to recycler view in XML

--- app/src/main/java/dev/sagar/eventconcertapp/MainActivity.kt
+++ app/src/main/java/dev/sagar/eventconcertapp/MainActivity.kt
@@ -3,61 +3,63 @@ package dev.sagar.eventconcertapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scope = rememberCoroutineScope()
-
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Snackbar {
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
+            val scrollState = rememberScrollState()
+//            Column(
+//                modifier = Modifier.verticalScroll(state = scrollState)
+//            ) {
+//                for (i in 1..40) {
+//                    Text(
+//                        text = "Item no. $i",
+//                        fontSize = 24.sp,
+//                        fontWeight = FontWeight.Bold,
+//                        textAlign = TextAlign.Center,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(24.dp)
+//                    )
+//                }
+//            }
+            LazyColumn {
+                itemsIndexed(listOf("A", "B", "C", "D")){ index, item ->
+                    Text(
+                        text = "Item no. $item",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(3.dp)
-                    ) {
-                        TextField(
-                            value = textFieldState,
-                            label = {
-                                Text(text = "Enter your name")
-                            },
-                            onValueChange = {
-                                textFieldState = it
-                            },
-                            singleLine = true,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Button(onClick = {
-                            scope.launch {
-                                scaffoldState.snackbarHostState.showSnackbar(message = "Hello $textFieldState")
-                            }
-                        }) {
-                            Text(text = "Please greet me")
-                        }
-                    }
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                    )
                 }
+//                items(count = 5000) {
+//                    Text(
+//                        text = "Item no. $it",
+//                        fontSize = 24.sp,
+//                        fontWeight = FontWeight.Bold,
+//                        textAlign = TextAlign.Center,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(24.dp)
+//                    )
+//                }
             }
+
         }
     }
 }
